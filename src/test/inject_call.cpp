895c1440aa50850d576101681432d0f7cfea5bfe
Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>
2018-02-25 03:01:51 -0600

    Solid fix in place.
    Signed-off-by: Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>

--- src/test/inject_call.cpp
+++ src/test/inject_call.cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 
 int main(int argc, char* argv[]) {
+	printf("\nFixed after analysis by RandomReader in freenode ##c++");
 	printf("\nInject_call: start.\nList of argv[]\n");
 	for (int i=0; i<argc; i++) {printf("\t%i  %s\n", i, argv[i]);}
 //  --------------------------------------------------------------
@@ -20,7 +21,8 @@ int main(int argc, char* argv[]) {
 
 	std::ostringstream strs;
 	strs << nHeight;
-	static const char *cstr[9] = { strs.str().c_str() };
+	static std::string stable_string = strs.str();
+	static const char *cstr[9] = { stable_string.c_str() };
 	printf("\nnHeight is %s\n", *cstr);
 	const char *argv_local[] = { \
 		"bitcoind", \
@@ -39,10 +41,10 @@ int main(int argc, char* argv[]) {
         string strMethod = argv_local[1];
         std::vector<std::string> \
 		strParams(&argv_local[2], &argv_local[argc_local]);
-	printf("\nItem Nr. 0 should be %i.  This code fails.\n", nHeight);
+	printf("\nItem Nr. 0 should be %i.  This code now works.\n", nHeight);
 	for (int i=0; i<argc_local-2; i++) \
 		{printf("\t%i  %s\n", i, strParams[i].c_str());}
-	printf("\nRe-print the argc array.  This should be unchanged.  How did this happen?\n");
+	printf("\nRe-print the argc array.  This should be unchanged.\n");
 	for (int i=0; i<argc_local; i++) \
 		{printf("\t%i  %s\n", i, argv_local[i]);}
 //  --------------------------------------------------------------
